8b39d305efb3921996d6133f6acfd1f6f4f6e817
mfraga710 <82245582+mfraga710@users.noreply.github.com>
2024-02-04 16:48:54 -0300

    WIP: agregado lectura de QR

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnDestroy} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {IonicModule, NavController} from '@ionic/angular';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+
+
 import {Toast} from "../../utils/toast";
 
 @Component({
@@ -11,15 +14,33 @@ import {Toast} from "../../utils/toast";
   standalone: true,
   imports: [IonicModule, CommonModule, FormsModule]
 })
-export class HomePage  {
+export class HomePage implements OnDestroy{
   datetime: any;
-
+  isScanActive : boolean = false;
 
   constructor(private navCtrl: NavController) {
   }
 
-  readQr(){
-    console.log('Open camera')
+
+  async stopScanner(){
+     await BarcodeScanner.stopScan();
+     this.isScanActive = false;
+  }
+  async readQr(){
+
+    this.isScanActive = true;
+    await BarcodeScanner.checkPermission({ force: true });
+    const result = await BarcodeScanner.startScan();
+    console.log('Open camera');
+
+
+    if (result.hasContent) {
+      // Aquí puedes hacer lo que desees con la información del código QR
+      // por ejemplo, enviarlo a una API, navegar a una página específica, etc.
+      alert('Código QR escaneado: ' + result.content);
+
+    }
+    this.isScanActive = false;
   }
 
   openProfile(){
@@ -31,6 +52,11 @@ export class HomePage  {
     console.log('Open My Orders')
     this.navCtrl.navigateRoot('/orders', {animated: true}).then()
   }
+
+  ngOnDestroy(): void {
+    BarcodeScanner.stopScan();
+  }
+
 
 
 }
